d4475783888ce674618cffe0ec47618c059efba9
Eric Daoud <daoudattoyan.eric@gmail.com>
2022-02-01 15:57:27 +0100

    add comments

--- application/app.py
+++ application/app.py
@@ -10,11 +10,11 @@ assets = Environment(app)
 assets.url = app.static_url_path
 
 scss = Bundle(
-    "assets/main.scss",
-    filters="libsass",
-    output="css/scss-generated.css"
+    "assets/main.scss",  # 1. will read this scss file and generate a css file based on it
+    filters="libsass",   # 2. using this filter: https://webassets.readthedocs.io/en/latest/builtin_filters.html#libsass
+    output="css/scss-generated.css"  # 3. and output the generated .css file in the static/css folder
 )
-assets.register("scss_all", scss)
+assets.register("scss_all", scss)  # 4. register the generated css file, to be used in Jinja templates (see base.html)
 
 # app routes
 @app.route("/")
